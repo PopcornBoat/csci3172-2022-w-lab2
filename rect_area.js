2a3334e0651c259897745ffb91ccb82c62c10aa3
PopcornBoat <PopcornBoat@github.com>
2022-01-21 13:31:45 -0400

    Q(c) done

---
+++ rect_area.js
@@ -0,0 +1,15 @@
+/*
+    @author: Yuxuan Wang
+    @email: yx703587@dal.ca
+    @descp: CSCI 3172 Lab2 rect_area.js 
+*/
+
+// Q(c): calculate the area of rectangle
+function rectArea(length, width){
+    let area = length * width;
+    console.log(area);
+    return area;
+}
+
+var length = 20, width = 15;
+rectArea(20,15);
